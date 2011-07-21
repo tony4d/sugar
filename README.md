688dc7195a50e3f26c2435a6966485852139aba0
Tony Ford <tony4d@gmail.com>
2011-07-21 01:20:51 -0700

    Added new ad server mode to sugar, supporting O(1) ad serving using jsonp. Only ign adwrapper is supported for now, but easily extensible for any ad server. Also updated docs.

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
+# What is Sugar?
+
+Sugar is a flexible and powerful client-side display ad solution that allows you to take control of the ads in your web pages.
+
+## Control
+1. Sugar allows you to render display ads during page load, at different dom events, and even after onload. Since sugar uses friendly iframes for ad containers you do not need to worry about ads that contain document.write over-writing your pages after onload. Now you can monetize rich ajax applications.
+
+## Performance
+1. As noted under control, since ad rendering can be completely decoupled from the page rendering you are able to defer ad loads so that users get to the content in your web pages first. The key is the friendly iframe containers that give the ultimate flexibility to load ads asynchronously in your web pages.
+
+2. In ad server mode, sugar is able to make a single http request to fetch all ads at once. For example, if you have a web page with 3 display ads then sugar will eliminate 2 client side calls to the ad server. Effectively, ad serving becomes O(1) vs traditional O(n).
+
+3. Ad load performance analytics is built in. As a configurable option you can send ad performance data into google analytics.
+
+## Flexibility
+1. Using a simple configuration sugar can work with either traditional js embeds for each ad or in ad server mode using jsonp where a single http request fetches all ads at once.
+
+2. Sugar is extensible so you can easily define your ad server if it's not supported. Currently sugar supports these ad servers out of the box: IGN proprietary Ad Wrapper.
+
+
 # Enabling sugar in your web pages
 
-* First, add sugarfif.html to the root of your domain(s) where you serve your html pages
+* First, add sugarfif.html to the root of your domain(s) where you serve your html pages. Note, you should set cache control max-age to a very high value for this document. You do not want clients downloading this ore than once. If you're using apache here is a sample configuration that would work well:
+
+```
+<Files sugarfif.html>
+  Header set Cache-Control max-age=31536000
+</Files>
+```
 
 * Include the js library
 
@@ -8,12 +34,21 @@
 <script src="sugarads.js"></script>
 ```
 
-* Define your js ad urls
+* If you want to use traditional js embeds for O(n) ad serving you need to set the adServer property to false and define your js ad urls:
 
 ```html
 <script>
+SugarAds.adServer = false;
 SugarAds.adJsUrls['728x90'] = "http:\/\/example.com\/ad.js?size=728x90&var1=val1&var2=val2";
 SugarAds.adJsUrls['300x250'] = "http:\/\/example.com\/ad.js?size=300x250&var1=val1&var2=val2";
+</script>
+```
+
+* If you want to use ad server mode and fetch all ads using jsonp for O(1) ad serving you need to set the adServer property to the appropriate supported ad server:
+
+```html
+<script>
+SugarAds.adServer = 'ignadwrapper';
 </script>
 ```
 
@@ -30,10 +65,7 @@ SugarAds.adJsUrls['300x250'] = "http:\/\/example.com\/ad.js?size=300x250&var1=va
 <script>
 // Render both ads
 SugarAds.renderAds(["728x90","300x250"]);
-// Or, pass nothing and all ads will render
-SugarAds.renderAds();
-
-// Render just one
+// Render just one ad
 SugarAds.renderAds(["728x90"]);
 // For just one ad, a string is fine too
 SugarAds.renderAds("728x90");
--- sugarads.js
+++ sugarads.js
@@ -1,75 +1,149 @@
 SugarAds = {
-
     adDivCssClassName: 'sugarad',
     adDivIdPrefix: 'sugarad-',
+    adIframeIdSuffix: '-iframe',
     adDivWidthAttrib: 'data-sugar-ad_width',
     adDivHeightAttrib: 'data-sugar-ad_height',
-    analytics: true,
-    fifurl: '/sugarfif.html?1',
-    stitialTimeout: 10000,
-    randNum: 0,
-    randLength: 12,
-    ads: {},
+    analytics: false,
+    fifurl: '/sugarfif.html?2',
+    urlRandLength: 12,
+    urlRandNum: 0,
+    adsData: {},
+    adCreatives: {},
     adJsUrls: {},
+    stitialOverlayId: 'sugarad-stitial-overlay',
+    stitialHtmlElementClass: 'sugarad-stitial-open',
+    stitialTimeout: 10000,
+    stitialAdType: false,
+    adServer: false,
+    adServers: {
+        ignadwrapper: {
+            getUrl: function (sugar, adsToFetch) {
+                var data = sugar.adsData;
+                var url = 'http://wrapper.ign.com/services/ads/pagetype/'+data.pagetype+'/sizes/'+adsToFetch.toString()+'.js?callback=?';
 
-    renderAds: function (adTypes) {
-        var adsToRender = [];
-        // Do some input validation first
-        switch (this._typeOf(adTypes)) {
-            case 'undefined':
-                // render all ads defined in adJsUrls
-                for (adType in this.adJsUrls) {
-                    adsToRender.push(adType);
+                // Propagate specials from the query string of the page
+                var currentPageUrl = document.location;
+                if (currentPageUrl.search.indexOf('special') != -1) {
+                    var queryParams = currentPageUrl.search.substr(1).split('&');
+                    for (var i = 0, len = queryParams.length; i < len; i++) {
+                        if (queryParams[i].indexOf('special') != -1) {
+                            url += '&'+queryParams[i];
+                        }
+                    }
                 }
-                break;
-            case 'string':
-                // turn this into an array and we'll process it like that
-                adTypes = new Array(adTypes);
-            case 'array':
-                // only dealing with the array of types we were passed
-                for (var i = 0, len = adTypes.length; i < len; i++) {
-                    adsToRender.push(adTypes[i]);
+
+                // Add all the data on to the url
+                for (var param in data) {
+                    // the pagetype is already in the url so we don't need to put it in the query string
+                    if (param == 'pagetype') {
+                        continue;
+                    }
+                    var val = data[param];
+                    // values can be arrays
+                    if (sugar._isArray(val)) {
+                        for (var i = 0, len = val.length; i < len; i++) {
+                            url += '&'+param+'='+encodeURIComponent(val[i]);
+                        }
+                    } else {
+                        url += '&'+param+'='+encodeURIComponent(val);
+                    }
                 }
-                break;
-            default:
-                console.warn('renderAds() expects no argument (refresh all ad types), a string (a single ad type), or an array (one or more ad types)');
+
+                // always add the referrer if there is one
+                var referrer = document.referrer;
+                if (referrer != '') {
+                    url += '&r='+encodeURIComponent(referrer);
+                }
+                return url;
+            },
+            jsonpCallback: function (sugar, adsToRender) {
+                return function (json) {
+                    var stitialSize = 'prestitial';
+                    // Setup the ad creatives in the ads hash
+                    for (var i = 0, len = json.length; i < len; i++) {
+                        // Figure out if you have a stitial
+                        if (json[i].size.substr(-7) == 'stitial') {
+                            if (json[i].impressionTracker == false) {
+                                // we have a real stitial ad
+                                sugar.stitialAdType = stitialSize;
+                                sugar.adCreatives[stitialSize] = json[i].creative;
+                            } else {
+                                sugar.adCreatives[stitialSize] = '';
+                            }
+                        } else {
+                            sugar.adCreatives[json[i].size] = json[i].creative;
+                        }
+                    }
+                    sugar._fetchAdsCallback(adsToRender);
+                };
+            }
+        },
+        dfp: {
+            // TODO: setup dfp here
+            getUrl: function (adsToFetch, data) {
+                return 'http://example.com/ads/'+adsToFetch.toString()+'.jsonp';
+            },
+            jsonpCallback: function (sugar, adsToRender) {
+                return function (json) {
+                    // populate sugar.adCreatives with the ad creatives
+                };
+            }
         }
-        this._flushAndPlaceAds(adsToRender);
     },
 
-    initializePrestitial: function (continueLinkId, overlayId) {
-        // First set a style on the page's html element so we have some style control for the overlay
-        // This allows you to do things like hide flash objects so they don't screw your zindex
-        document.getElementsByTagName('html')[0].className = 'sugarad-stitial-open';
+    renderAds: function (adsToRender) {
+        // This function expects an array of ad types, but gracefully handle a string anyway
+        if (typeof adsToRender == 'string') {
+            adsToRender = new Array(adsToRender);
+        }
 
-        // Set the timeout for the prestitial
-        this.prestitialTimer = setTimeout(
-            function() { SugarAds.hidePrestitial(overlayId); },
+        // Time to make some $
+        if (this.adServer != false) {
+            this._renderAdCreatives(adsToRender);
+        } else {
+            this._renderAdJsUrls(adsToRender);
+        }
+    },
+
+    showStitial: function () {
+        // Set a style on the page's html element so we have some style control for the overlay
+        // This allows you to do things like hide flash objects so they don't screw your zindex
+        var htmlElement = document.getElementsByTagName('html')[0];
+        var stitialOverlayElement = document.getElementById(this.stitialOverlayId);
+        htmlElement.className = this.stitialHtmlElementClass;
+        stitialOverlayElement.style.display = 'block'; // display the overlay
+
+        // Set the timeout for the stitial
+        this.stitialSetTimeout = setTimeout(
+            (function(sugar) {
+                return function() { sugar.hideStitial(); };
+            })(this),
             this.stitialTimeout
         );
     },
 
-    hidePrestitial: function (overlayId) {
+    hideStitial: function () {
         // Clear the timer in case the overlay was manually closed
-        clearTimeout(this.prestitialTimer);
+        clearTimeout(this.stitialSetTimeout);
 
-        // Remove special style for the page
-        document.getElementsByTagName('html')[0].className = '';
-
-        // Hide the prestitial overlay
-        document.getElementById(overlayId).style.display = 'none';
+        // Now we need to remove the special style for the page
+        var htmlElement = document.getElementsByTagName('html')[0];
+        var stitialOverlayElement = document.getElementById(this.stitialOverlayId);
+        htmlElement.className = '';
+        stitialOverlayElement.style.display = 'none'; // Hide the overlay
 
         // Fire page ads now
         this.renderAdsDelayedByStitial();
     },
 
     renderAdsDelayedByStitial: function () {
-        // this is a callback function to be defined by stitial code. Only it knows which ads to render
+        // this is a callback function that will be defined in the case of a stitial
     },
 
     setFifDim: function (frameElement,w,h) {
-         frameElement.fifwidth=w;
-         frameElement.fifheight=h;
+        if (typeof w == 'number') frameElement.fifwidth = w;
+        if (typeof h == 'number') frameElement.fifheight = h;
     },
 
     fifOnload: function (frameWindow) {
@@ -83,66 +157,78 @@ SugarAds = {
                 timerStop - frameWindow.sugarTimerStart
             ]);
         }
-        // track event call
-        this.stylizeAdContainer(frameWindow);
+        this._stylizeAdContainer(frameWindow);
     },
 
-    stylizeAdContainer: function (frameWindow) {
+    _stylizeAdContainer: function (frameWindow) {
         var frameElement = frameWindow.frameElement;
-        frameElement.width = frameElement.fifwidth;
-        frameElement.height = frameElement.fifheight;
-
-        // Setting dimensions on the sugar div will make certain the size stays static
-        // for client-side refreshes while the iframe is destroyed and replaced, avoiding page bounce
-        frameElement.parentNode.style.width = ''+frameElement.fifwidth+'px';
-        frameElement.parentNode.style.height = ''+frameElement.fifheight+'px';
 
         // if there is no content make sure we don't show it at all
-        // This helps avoid weird vertical padding issues like margin on the sugar div
-        if (frameElement.fifwidth == 0 && frameElement.fifheight == 0) {
+        // Note, this eliminates page layout bouncing caused by vertical padding or margin on the div
+        if (frameElement.fifwidth == 0
+            && frameElement.fifheight == 0
+            && frameElement.parentNode.style.display != 'none') {
             frameElement.parentNode.style.display = 'none';
         } else {
-            frameElement.parentNode.style.display = 'block';
+            // Set dimensions on the iframe and then the div. This is required for client-side ad refreshes
+            // We avoid page layout bouncing when the iframe is destroyed and replaced
+            // Note, order is important, do the iframe before the div to avoid any bouncing
+            frameElement.style.cssText += ';width:'+frameElement.fifwidth+'px;height:'+frameElement.fifheight+'px;';
+            // Note that we actually change the display property here too just in case it is hidden right now
+            frameElement.parentNode.style.cssText = 'width:'+frameElement.fifwidth+'px;height:auto;display:block;';
         }
     },
 
     _flushAndPlaceAds: function (adTypes) {
-        this._randomizeAdJsUrls();
-        var iframeSrcUrl = (this.fifurl != undefined && this.fifurl != '') ? this.fifurl : '/sugarfif.html';
-
         for (var i = 0, len = adTypes.length; i < len; i++) {
             var adType = adTypes[i];
-            if (!this.adJsUrls.hasOwnProperty(adType)) {
-                console.warn('Failed to render sugar ad. No ad js url has been defined for ad type "'+adType+'".');
+
+
+            if (this.adServer == false && !(adType in this.adJsUrls)) {
+                this._warn('Failed to render sugar ad. No ad js url has been defined for ad type "'+adType+'".');
+                continue;
+            }
+            if (this.adServer != false && !(adType in this.adCreatives)) {
+                this._warn('Failed to render sugar ad. No ad creative has been defined for ad type "'+adType+'".');
                 continue;
             }
             // fire away
-            this._createAndAppendFriendlyIframe(
-                adType,
-                iframeSrcUrl,
-                this.adJsUrls[adType]
-            );
+            this._createAndAppendFriendlyIframe(adType);
         }
     },
 
     // Create an iframe in the given element with the specified values
-    _createAndAppendFriendlyIframe: function (adType, iframeSrcUrl, adJsUrl) {
+    _createAndAppendFriendlyIframe: function (adType) {
         var adDivDomId = this.adDivIdPrefix+adType;
         var adContainer = document.getElementById(adDivDomId);
-        if (this._typeOf(adContainer) == 'null') {
-            console.warn('Failed to render sugar ad. No dom element with id "'+adDivDomId+'" exists.');
+        if (typeof adContainer == 'undefined' || adContainer == null) {
+            this._warn('Failed to render sugar ad. No dom element with id "'+adDivDomId+'" exists.');
             return false;
+        }
+
+        // avoid page bounce by saving the previous iframe height
+        var adContainerIframe = document.getElementById(adDivDomId+this.adIframeIdSuffix);
+        if (typeof adContainerIframe != 'undefined' && adContainerIframe != null
+            && typeof adContainerIframe.fifheight != 'undefined' && adContainerIframe.fifheight != null) {
+            adContainer.style.height = adContainerIframe.fifheight+'px';
         }
 
         // Clear the ad container element contents
         adContainer.innerHTML = '';
 
+        // If we fetched an ad creative and there is no ad then we should not create a new iframe at all
+        if (this.adServer != false && this.adCreatives[adType] == '') {
+            // make sure to remove the ad from the hash so it is not used again though
+            delete this.adCreatives[adType];
+            return;
+        }
+
         // create iframe
-        var adIframeId = adDivDomId+'-iframe';
+        var adIframeId = adDivDomId+this.adIframeIdSuffix;
         var iframe = document.createElement('iframe');
         iframe.id = adIframeId;
         iframe.name = adIframeId;
-        iframe.src = iframeSrcUrl;
+        iframe.src = this.fifurl;
         iframe.style.border = '0px';
         iframe.width = 0;
         iframe.height = 0;
@@ -151,25 +237,85 @@ SugarAds = {
         iframe.fifwidth = adContainer.getAttribute(this.adDivWidthAttrib);
         iframe.fifheight = adContainer.getAttribute(this.adDivHeightAttrib);
         iframe.adtype = adType;
-        iframe.adjsurl = adJsUrl;
+        if (this.adServer != false) {
+            iframe.adcreative = this.adCreatives[adType];
+            // remove ad from the hash so it is not used again
+            delete this.adCreatives[adType];
+        } else {
+            iframe.adjsurl = this.adJsUrls[adType];
+        }
         // of course IE needs non-standard styling
-        if (navigator.userAgent.indexOf("MSIE") == -1) {
+        if (navigator.userAgent.indexOf("MSIE") != -1) {
             iframe.frameBorder = '0';
-            iframe.allowtransparency = 'true';
+            iframe.allowTransparency = 'true';
         }
         // Append the iframe into ad container div
         adContainer.appendChild(iframe);
     },
 
-    _randomizeAdJsUrls: function () {
-        if (this.randNum == 0) {
-            this.randNum = Math.floor(Math.random()*Math.pow(10, this.randLength));
+    _renderAdCreatives: function (adsToRender) {
+        var adsToFetch = [];
+        // We're only gonna fetch ads we don't have already
+        for (var i = 0, len = adsToRender.length; i < len; i++) {
+            if (!(adsToRender[i] in this.adCreatives)) {
+                adsToFetch.push(adsToRender[i]);
+            }
+        }
+
+        // Only fetch ads if we have some to fetch
+        if (adsToFetch.length > 0) {
+            this._jsonp(
+                this.adServers[this.adServer].getUrl(this, adsToFetch),
+                this.adServers[this.adServer].jsonpCallback(this, adsToRender)
+            );
         } else {
-            this.randNum++;
+            this._fetchAdsCallback(adsToRender);
+        }
+    },
+
+    _renderAdJsUrls: function (adsToRender) {
+        this._randomizeAdJsUrls();
+        this._flushAndPlaceAds(adsToRender);
+    },
+
+    _fetchAdsCallback: function(adsToRender) {
+        if (this.stitialAdType != false) {
+            // When there is a stitial, first setup a callback that will render
+            // all other ads when the stitial is hidden
+            var delayedAdsToRender = [];
+            for (var i = 0, len = adsToRender.length; i < len; i++) {
+                var adToRender = adsToRender[i];
+                // we're creating an array of ads to render not including the stitial
+                if (adToRender != this.stitialAdType) {
+                    delayedAdsToRender.push(adToRender);
+                }
+            }
+            this.renderAdsDelayedByStitial = (function (sugar) {
+                return function () { sugar._flushAndPlaceAds(delayedAdsToRender); };
+            })(this);
+
+            // now, just flush and place the stitial ad
+            this._flushAndPlaceAds([this.stitialAdType]);
+            // make sure the stitial doesn't run again
+            this.stitialAdType = false;
+
+            // and finally show the stitial
+            this.showStitial();
+        } else {
+            // if no stitial ad to show just render all ads
+            this._flushAndPlaceAds(adsToRender);
+        }
+    },
+
+    _randomizeAdJsUrls: function () {
+        if (this.urlRandNum == 0) {
+            this.urlRandNum = Math.floor(Math.random()*Math.pow(10, this.urlRandLength));
+        } else {
+            this.urlRandNum++;
         }
         var randQsKey = 'sugar-rand';
-        var newQsKeyValue = randQsKey + '=' + this.randNum;
-        var searchRegexp = new RegExp(randQsKey + '=\\d{' + this.randLength + '}', 'g');
+        var newQsKeyValue = randQsKey + '=' + this.urlRandNum;
+        var searchRegexp = new RegExp(randQsKey + '=\\d{' + this.urlRandLength + '}', 'g');
 
         for (adType in this.adJsUrls) {
             var adJsUrl = this.adJsUrls[adType];
@@ -190,20 +336,43 @@ SugarAds = {
         }
     },
 
-    // Enhanced typeof that deals with null and arrays, even in different frames. Always returns string value
-    _typeOf: function (value) {
-        var s = typeof value;
-        if (s === 'object') {
-            if (value) {
-                if (typeof value.length === 'number' &&
-                        !(value.propertyIsEnumerable('length')) &&
-                        typeof value.splice === 'function') {
-                    s = 'array';
-                }
-            } else {
-                s = 'null';
-            }
+    _jsonp: function(url, callback) {
+        // do some validation on the input
+        if (url.indexOf('=?') == -1) {
+            this._warn('The sugar jsonp url must specify the callback function in the query string i.e. callback=?');
+            return;
         }
-        return s;
+        if (typeof callback != 'function') {
+            this._warn('The sugar jsonp callback must be a function');
+            return;
+        }
+        var randFunctionName = 'jsonp'+Math.floor(Math.random()*10000000000);
+
+        // Setup the callback function that will be called when the external script executes
+        // Note the function ends up cleaning itself up since we just need to call the real callback
+        window[randFunctionName] = function(json) {
+            callback(json);
+            // ie doesn't allow deleting props on thw window object
+            try {
+                delete window[randFunctionName];
+            } catch(e) {
+                window[randFunctionName] = undefined;
+            }
+        };
+
+        // Fetch the external script and get the json
+        var jsonp = document.createElement('script');
+        jsonp.src = url.split('=?').join('='+randFunctionName);
+        document.getElementsByTagName('head')[0].appendChild(jsonp);
+    },
+
+    _isArray: function (o) {
+      return Object.prototype.toString.call(o) === '[object Array]';
+    },
+
+    _warn: function (msg) {
+        if (window.console && console.warn) {
+            console.warn(msg);
+        }
     }
 }
